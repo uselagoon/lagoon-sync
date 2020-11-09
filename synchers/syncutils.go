e3b52b7a79828877c446999f68853cdff8297e4c
Blaize Kaye <blaize.kaye@amazee.com>
2020-11-10 11:37:39 +1300

    Adds first pass at executing commands

--- synchers/syncutils.go
+++ synchers/syncutils.go
@@ -1,9 +1,11 @@
 package synchers
 
 import (
+	"bytes"
 	"errors"
 	"fmt"
 	"gopkg.in/yaml.v2"
+	"os/exec"
 )
 
 // UnmarshallLagoonYamlToLagoonSyncStructure will take a bytestream and return a fully parsed lagoon sync config structure
@@ -16,10 +18,32 @@ func UnmarshallLagoonYamlToLagoonSyncStructure(data []byte) (SyncherConfigRoot, 
 	return lagoonConfig, nil
 }
 
+const ShellToUse = "bash"
+
+func Shellout(command string) (error, string, string) {
+	var stdout bytes.Buffer
+	var stderr bytes.Buffer
+	cmd := exec.Command(ShellToUse, "-c", command)
+	cmd.Stdout = &stdout
+	cmd.Stderr = &stderr
+	err := cmd.Run()
+	return err, stdout.String(), stderr.String()
+}
+
 
 func SyncRunRemote(syncer Syncer) error {
-	fmt.Print("I'm going to be running the following: ")
 	println(syncer.GetRemoteCommand())
+
+	execString := fmt.Sprintf("ssh -t -o \"UserKnownHostsFile=/dev/null\" -o \"StrictHostKeyChecking=no\" -p 32222 %v@ssh.lagoon.amazeeio.cloud '%v'",
+	"amazeelabsv4-com-dev", "ls")
+
+	err, outstring, errstring := Shellout(execString)
+
+	if err != nil {
+		fmt.Println(errstring)
+		return err
+	}
+	fmt.Println(outstring)
 	return nil
 }
 
